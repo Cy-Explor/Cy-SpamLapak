8f6db7d54ef70886fc5c62f5cbbbf912cda7224b
Cy-Explor <51297931+Cy-Explor@users.noreply.github.com>
2019-06-03 19:07:41 +0700

    Update gasboejank.php

--- gasboejank.php
+++ gasboejank.php
@@ -1,20 +1,16 @@
-#!/system/xbin/bash
 <?php
-clear
-toilet -f slant --gay "Cy-SpamLapak"
-sleep 1
 echo "COPYRIGHT : Cy-Rxplor\n\n";
-echo "Nomor Target?\nInput : ";
+echo "Masukan Nomor Targetnya boejank?\nInput : ";
 $nomer = trim(fgets(STDIN));
 if(strlen($nomer)==11){
 	$nomer = str_replace("0","62".$nomer);
 }elseif(strlen($nomer)>12){
 	$nomer = str_replace("62","0",$nomer);
 }
-echo "Target: $nomer (y/n)";
+echo "Targetnya Betul Ngak Boejank: $nomer (y/n)";
 $cek = trim(fgets(STDIN));
 if($cek=="n") exit("Stopped!\n");
-echo "Jumlah?\nInput : ";
+echo "Jumlahnya berapa Boejank?\nInput : ";
 $jumlah = trim(fgets(STDIN));
 for($a=0;$a<$jumlah;$a++) {
 	$rand1 = md5(rand(12345678,98765432));
@@ -55,5 +51,5 @@ Cookie: identity=".$rand1."; browser_id=".$rand2."; _ga=GA1.2.1024758930.1531960
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $ar);
 	$asw = curl_exec($ch);
 	curl_close($ch);
-	print $a.$nomer." [Sending]\n";
+	print $a.$nomer." [Dah Terkirim Bujank]\n";
 }
